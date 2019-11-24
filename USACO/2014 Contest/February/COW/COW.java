37c1405044aa88046b501f86fd1c287d810186ca
threecentscoding <58013004+threecentscoding@users.noreply.github.com>
2019-11-23 22:56:54 -0500

    Update COW.java

--- USACO/2014 Contest/February/COW/COW.java
+++ USACO/2014 Contest/February/COW/COW.java
@@ -1,1 +1,43 @@
+//Import Statements
+import java.util.*;
+import java.io.*;
 
+public class COW
+{
+      public static void main(String[] args) throws IOException
+      {
+            BufferedReader br = new BufferedReader(new FileReader("barnjump.in"));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("barnjump.out")));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+        
+            int length = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+        
+            long numC;
+            long numO;
+            long numW;
+        
+            char[] characters = str.charArray();
+          
+            for (int i = 0; i < length; i++)
+            {
+                 if (char[i] = 'C')
+                 {
+                      numC++;
+                 }
+                 else if (char[i] = 'O')
+                 {
+                      numO += numC;
+                 }
+                 else
+                 {
+                      numW += numO;
+                 }
+            }
+        
+            out.println(numW);
+            out.close();
+      }
+}
+                    
