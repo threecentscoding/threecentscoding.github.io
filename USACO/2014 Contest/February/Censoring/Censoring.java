91ee0492bf9753c67b60cbeb81c3f66c75f01bbe
threecentscoding <58013004+threecentscoding@users.noreply.github.com>
2019-11-23 22:00:43 -0500

    Update Censoring.java

--- USACO/2014 Contest/February/Censoring/Censoring.java
+++ USACO/2014 Contest/February/Censoring/Censoring.java
@@ -1,1 +1,25 @@
+//Import Statements
 import java.util.*;
+import java.io.*;
+
+public class Censoring
+{   
+      public static void main(String[] args) throws IOException
+      { 
+            BufferedReader f = new BufferedReader(new FileReader("censor.in"));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("censor.out")));
+            StringTokenizer st = new StringTokenizer(f.readLine());
+        
+            String explicitWord = st.nextToken();
+            st = new StringTokenizer(f.readLine());
+            String removeWord = st.nextToken();
+        
+            while(explicitWord.indexOf(removeWord) != -1)
+            {
+                  explicitWord.replaceAll(removeWord, "");
+            }
+        
+            out.println(explicitWord);
+            out.close();
+      }
+}
