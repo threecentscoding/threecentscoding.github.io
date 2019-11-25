2275ce3656e1fa088e9a26b8710d75fa43fa2cb2
threecentscoding <58013004+threecentscoding@users.noreply.github.com>
2019-11-25 18:47:53 -0500

    Update COW.java

--- USACO/2014 Contest/February/COW/COW.java
+++ USACO/2014 Contest/February/COW/COW.java
@@ -6,27 +6,27 @@ public class COW
 {
       public static void main(String[] args) throws IOException
       {
-            BufferedReader br = new BufferedReader(new FileReader("barnjump.in"));
-            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("barnjump.out")));
+            BufferedReader br = new BufferedReader(new FileReader("cow.in"));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cow.out")));
             StringTokenizer st = new StringTokenizer(br.readLine());
         
             int length = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
         
-            long numC;
-            long numO;
-            long numW;
+            long numC = 0;
+            long numO = 0;
+            long numW = 0;
         
-            char[] characters = str.charArray();
+            char[] characters = str.toCharArray();
           
             for (int i = 0; i < length; i++)
             {
-                 if (char[i] = 'C')
+                 if (char[i] == 'C')
                  {
                       numC++;
                  }
-                 else if (char[i] = 'O')
+                 else if (char[i] == 'O')
                  {
                       numO += numC;
                  }
